b62291b980fe98b93162319ae14a87e3dd50cd7e
Volodymyr Savaryn <volodymyr.savaryn@irco.com>
2020-03-02 17:49:45 +0200

    Updates

---
+++ precache-manifest.fe75a3b15c6b0f29502f4fb9ea8b25db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b1e6ec0a636db13db6fb7cfe73358e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1be5db787210f19fd8e6",
+    "url": "./static/css/main.73b46aa8.chunk.css"
+  },
+  {
+    "revision": "440b776fd947060527ec",
+    "url": "./static/js/2.954b3d2b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.954b3d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be5db787210f19fd8e6",
+    "url": "./static/js/main.9b3fbff4.chunk.js"
+  },
+  {
+    "revision": "238001c50fe4b49a6b6e",
+    "url": "./static/js/runtime-main.d759505f.js"
+  }
+]);
